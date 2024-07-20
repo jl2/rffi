d7e6ac22449d28648d60fdba44e6d65ce98460fd
Jeremiah LaRocco <jeremiah_larocco@fastmail.com>
2024-07-19 23:47:22 -0600

    Add equivalent Common Lisp for fibonacci

--- examples/simple-lib/src/lib.rs
+++ examples/simple-lib/src/lib.rs
@@ -28,7 +28,15 @@ mod tests {
     }
     
 }
+
 mod fib;
+
+
+// (defun fibonacci (n)
+//   (if (or (= n 0) (= n 1))
+//      1
+//      (+ (fibonacci (- n 1))
+//         (fibonacci (- n 2)))))
 
 fn fibonacci(n: u64) -> u64 {
     match n {
